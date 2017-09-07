f453ebd047ec05cae763a9fa1bd8d7cb44329f19
Jesse Epperson <Epperson.JesseE@gmail.com>
2017-09-07 18:34:36 +0800

    adding files to be tracked

---
+++ SEAL/config.h
@@ -0,0 +1,6 @@
+/* config.h.  Generated from config.h.in by configure.  */
+#define ENABLE_INTRIN 1
+#define ENABLE___BUILTIN_CLZLL 1
+#define ENABLE__ADDCARRY_U64 1
+#define ENABLE__SUBBORROW_U64 1
+#define ENABLE__MULX_U64 1
